bd8b9c3c955f02f5876e104cbf756301f5082ccb
lisper <leyapin@gmail.com>
2016-05-03 03:36:32 +0800

    add step_test1.ino

---
+++ step_test1/step_test1.ino
@@ -0,0 +1,21 @@
+#include <Stepper.h>
+
+// 这里设置步进电机旋转一圈是多少步
+#define STEPS 200
+
+// attached to设置步进电机的步数和引脚
+Stepper stepper(STEPS, 2, 3, 4, 5);
+
+
+void setup() {
+	stepper.setSpeed(100);
+}
+
+void loop () {
+	stepper.step (0);
+	delay (500);
+	stepper.step (200);
+	delay (500);
+}
+
+
